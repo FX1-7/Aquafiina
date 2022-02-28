5cb05c1033425ec2180c2ce1ef3a80e76508cad9
Keiranos <31273773+Keiranos@users.noreply.github.com>
2022-02-28 02:17:24 +0000

    EDPY config.py changes

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
-import tweepy
 import logging
-import os
 LOG_ID = 866985685470019604
 TWITTER_CHANNEL = 866987655770013696
 PREFIX = "!"
@@ -9,26 +7,13 @@ YELLOW = 0xFCE9AF
 GREEN = 0xB1D587
 MAIN = 0x83B942
 PERMS = 8
-MOD_ROLE = 866970453456846888, 866985346116616202
+MOD_ROLE = 875314348766167040, 866970453456846888, 866985346116616202
 MUTED = 867022751255887873
 BLUE = 0x1DA1F2
+GUILD_ID = 866970151873806366
+STARBOARD_ID = 867007259200716800
+COLOUR = 0xFFD700
+
+star_threshold = 6
 
 logger = logging.getLogger()
-
-def create_api():
-    consumer_key = os.getenv("CONSUMER_KEY")
-    consumer_secret = os.getenv("CONSUMER_SECRET")
-    access_token = os.getenv("ACCESS_TOKEN")
-    access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
-
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth, wait_on_rate_limit=True,
-        wait_on_rate_limit_notify=True)
-    try:
-        api.verify_credentials()
-    except Exception as e:
-        logger.error("Error creating API", exc_info=True)
-        raise e
-    logger.info("API created")
-    return api
